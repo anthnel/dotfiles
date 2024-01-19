a6814335c728f21faa83bbddc933a8bb99cd1213
Anthoni Nelli <anthoni.nelli@pm.me>
2024-01-19 21:05:05 +0100

    install.sh

---
+++ .bin/install.sh
@@ -0,0 +1,15 @@
+git clone --bare https://github.com/anthnel/dotfiles.git $HOME/.dotfiles
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    mkdir -p .config-backup
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+
